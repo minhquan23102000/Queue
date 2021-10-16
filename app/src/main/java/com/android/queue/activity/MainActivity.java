a5804f5464c79cd171fd2a1379cf000e0dc87eec
MinhQuan <minhquan23102000@gmail.com>
2021-10-16 12:24:11 +0700

    Add host room activity, and qr code showing layout

--- app/src/main/java/com/android/queue/activity/MainActivity.java
+++ app/src/main/java/com/android/queue/activity/MainActivity.java
@@ -7,19 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.android.queue.R;
+import com.android.queue.SessionManager;
 import com.google.android.material.button.MaterialButton;
+import com.android.queue.firebase.realtimedatabase.QueueDatabaseContract.UserEntry;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private MaterialButton logoutBtn;
     private MaterialButton createRoomBtn;
+    private SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Init session manager
+        sessionManager = new SessionManager(this);
         //Init all view in this activity
         logoutBtn = findViewById(R.id.logoutBtn);
         createRoomBtn = findViewById(R.id.hostBtn);
@@ -39,5 +43,19 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //If user is already create or join a room, navigate to room layout
+        Bundle userData = sessionManager.getUserData();
+        if (userData.getString(UserEntry.CURRENT_ROOM_ARM, null) != null) {
+            if (userData.getBoolean(UserEntry.IS_HOST_ARM, false)) {
+                Intent intent = new Intent(MainActivity.this, HostActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        }
     }
 }
