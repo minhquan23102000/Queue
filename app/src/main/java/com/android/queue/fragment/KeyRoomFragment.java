99f1ef5cfeadbf04dac4d86d42060e44142a8f4b
MinhQuan <minhquan23102000@gmail.com>
2021-10-17 11:16:01 +0700

    Complete use case save QR code to user's storage

--- app/src/main/java/com/android/queue/fragment/KeyRoomFragment.java
+++ app/src/main/java/com/android/queue/fragment/KeyRoomFragment.java
@@ -1,11 +1,23 @@
 package com.android.queue.fragment;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +28,8 @@ import android.widget.TextView;
 import com.android.queue.R;
 import com.android.queue.SessionManager;
 import com.android.queue.firebase.storage.FirebaseStorageRequester;
+import com.android.queue.utils.ImageSaver;
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +49,11 @@ public class KeyRoomFragment extends Fragment {
 
     //Init firebase storage requester
     private FirebaseStorageRequester firebaseStorageRequester;
+    //Init image saver to help to write qr into user storager
+    private ImageSaver imageSaver;
+    //Init context and activity
+    private Activity mActivity;
+    private Context mContext;
 
     public KeyRoomFragment() {
         // Required empty public constructor
@@ -55,6 +74,9 @@ public class KeyRoomFragment extends Fragment {
         }
         sessionManager = new SessionManager(getContext());
         firebaseStorageRequester = new FirebaseStorageRequester(getContext());
+        imageSaver = new ImageSaver(getContext());
+        mActivity = getActivity();
+        mContext = getContext();
     }
 
     @Override
@@ -72,7 +94,7 @@ public class KeyRoomFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull  View view, @Nullable  Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         String roomKey = sessionManager.getCurrentRoomKey();
 
@@ -81,6 +103,33 @@ public class KeyRoomFragment extends Fragment {
 
         //Fetch room key into text view
         roomKeyTv.setText(roomKey);
+
+
+        //On click to download qr code
+        downloadBtn.setOnClickListener(v -> {
+            BitmapDrawable drawable = (BitmapDrawable) qrCodeImgView.getDrawable();
+            Bitmap qrcode = drawable.getBitmap();
+
+            if (ImageSaver.isExternalStorageWritable() && ImageSaver.checkPermission(mContext)) {
+                imageSaver.setFileName(firebaseStorageRequester.initAnUniqueString() + ".png")
+                        .setExternal(true)
+                        .save(qrcode);
+
+                Snackbar.make(view, "Mã qr đã được lưu thành công", Snackbar.LENGTH_LONG)
+                        .setAction("Xem mã", v1 -> {
+                            Intent intent = new Intent();
+                            intent.setAction(android.content.Intent.ACTION_VIEW);
+                            intent.setType("image/*");
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+
+                        }).show();
+            } else {
+                ActivityCompat.requestPermissions(mActivity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 101);
+            }
+
+
+        });
 
     }
 }
