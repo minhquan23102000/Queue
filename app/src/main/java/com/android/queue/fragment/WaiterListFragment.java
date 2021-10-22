2d26b117215d8e4b3cb4a484674779aed087cabe
MinhQuan <minhquan23102000@gmail.com>
2021-10-22 20:00:43 +0700

    Thêm filter cho waiter list, lọc theo trạng thái, lọc theo số chờ lớn hơn số chờ hiện tại

--- app/src/main/java/com/android/queue/fragment/WaiterListFragment.java
+++ app/src/main/java/com/android/queue/fragment/WaiterListFragment.java
@@ -5,8 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -40,6 +43,7 @@ public class WaiterListFragment extends Fragment {
 
     private long currentWait;
 
+    private static final String[] FILTER_ITEMS = new String[]{"Tất cả", "Đã xong", "Đang chờ", "Mất lượt", "Rời hàng"};
 
     public static WaiterListFragment newInstance(long currentWait) {
         WaiterListFragment fragment = new WaiterListFragment();
@@ -72,18 +76,93 @@ public class WaiterListFragment extends Fragment {
         showAll = view.findViewById(R.id.showAll);
         filter = view.findViewById(R.id.filled_exposed_dropdown);
         listView = view.findViewById(R.id.list);
-
+        filter.setText("Tất cả", false);
         //Kick out adapter
         participantList = new ArrayList<>();
         adapter = new MyWaiterAdapter(view.getContext(), participantList);
         listView.setAdapter(adapter);
 
+        //Create layout for wait setting
+        ArrayAdapter<String> filterItemsAdapter = new ArrayAdapter<>(view.getContext(), R.layout.spinner_item_layout, FILTER_ITEMS);
+        filter.setAdapter(filterItemsAdapter);
+
+        //Set onclick listener for filter
+        filter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                adapter.clear();
+                adapter.addAll(applyFilter(participantList));
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        filter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                adapter.clear();
+                adapter.addAll(applyFilter(participantList));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Set on click listener for show all
+        showAll.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                adapter.clear();
+                adapter.addAll(applyFilter(participantList));
+                adapter.notifyDataSetChanged();
+            }
+        });
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private ArrayList<Participant> applyFilter(ArrayList<Participant> participantList) {
+        ArrayList<Participant> filterParticipantList;
+        long startWaiter = showAll.isChecked() ? 0 : currentWait;
+        String filterData = filter.getText().toString().trim();
+
+
+        if (filterData.equals(FILTER_ITEMS[1])) {
+            filterParticipantList = filterArray(ParticipantListEntry.STATE_IS_DONE, startWaiter, participantList);
+        } else if (filterData.equals(FILTER_ITEMS[2])) {
+            filterParticipantList = filterArray(ParticipantListEntry.STATE_IS_WAIT, startWaiter, participantList);
+        } else if(filterData.equals(FILTER_ITEMS[3])) {
+            filterParticipantList = filterArray(ParticipantListEntry.STATE_IS_SKIP, startWaiter, participantList);
+        } else if (filterData.equals(FILTER_ITEMS[4])) {
+            filterParticipantList = filterArray(ParticipantListEntry.STATE_IS_LEFT, startWaiter, participantList);
+        } else {
+            filterParticipantList = filterArray("", startWaiter, participantList);
+        }
+
+        for (Participant participant:
+             filterParticipantList) {
+            Log.d("WaiterList", "applyFilter: " + participant.waiterState + participant.waiterNumber);
+        }
+
+
+        return filterParticipantList;
+    }
+
+    private ArrayList<Participant> filterArray(String state, long startNumber, ArrayList<Participant> participantList) {
+        ArrayList<Participant> filterParticipantList = new ArrayList<>();
+        for (Participant p:
+             participantList) {
+            if (p.waiterNumber >= startNumber && (p.waiterState.equals(state) || state.isEmpty())) {
+                filterParticipantList.add(p);
+            }
+        }
+        return filterParticipantList;
     }
 
     private ValueEventListener listListener = new ValueEventListener() {
@@ -95,7 +174,8 @@ public class WaiterListFragment extends Fragment {
                     participantList.add(waiter.getValue(Participant.class));
                 }
             }
-            adapter.addAll(participantList);
+            adapter.clear();
+            adapter.addAll(applyFilter(participantList));
             adapter.notifyDataSetChanged();
         }
 
